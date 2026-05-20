337ac322a297e44611c1888825bbf6647cadd620
TanvirHassan-official <tanvirhassan0006@gmail.com>
2026-05-20 09:30:05 +0600

    Auth processing

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -23,5 +23,6 @@ import { betterAuth } from "better-auth";
 import { dash } from "@better-auth/infra";
 
 export const auth = betterAuth({
+    baseURL:process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
     plugins: [dash()],
 });
