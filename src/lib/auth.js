5258b84282712a53ee3a00953ee4283ea07b3fd5
TanvirHassan-official <tanvirhassan0006@gmail.com>
2026-05-20 09:22:55 +0600

    Auth processing

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -18,3 +18,10 @@
 //         }, 
 //     }
 // });
+
+import { betterAuth } from "better-auth";
+import { dash } from "@better-auth/infra";
+
+export const auth = betterAuth({
+    plugins: [dash()],
+});
